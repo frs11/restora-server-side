262f671b956d3e1ba374661836c8a5129b9e850b
frs11 <fayezurrahman017@gmail.cpm>
2023-11-22 12:21:51 +0600

    Fixed Cookies not being set to the browser cookies

--- index.js
+++ index.js
@@ -34,14 +34,14 @@ async function run() {
     // Auth API
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      // console.log(user);
+      console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false,
+          secure: true,
           sameSite: "none",
         })
         .send({ success: true });
